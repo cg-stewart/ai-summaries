ec10b404ecac8c39dbb7e4b55819eb9c5bb3c878
Cameron Stewart <newrgm@NEWRGM.attlocal.net>
2024-08-27 07:38:32 -0500

    setup and deployed project

---
+++ src/index.ts
@@ -0,0 +1,55 @@
+import { Hono } from 'hono';
+import { generateText, LanguageModel } from 'ai';
+import { createOpenAI, OpenAIProvider } from '@ai-sdk/openai';
+import * as Cheerio from 'cheerio';
+
+type Bindings = { OPENAI_API_KEY: string };
+type Variables = {
+	model: LanguageModel;
+	openai: OpenAIProvider;
+};
+
+const app = new Hono<{ Bindings: Bindings; Variables: Variables }>();
+
+app.use('*', async (c, next) => {
+	const openai = createOpenAI({ apiKey: c.env.OPENAI_API_KEY });
+	const model = openai('gpt-4o');
+
+	c.set('openai', openai);
+	c.set('model', model);
+
+	return next();
+});
+
+const getPage = async (url: string) => {
+	const response = await fetch(url);
+	return await response.text();
+};
+
+const parsePageContent = async (text: string) => {
+	const $ = Cheerio.load(text);
+	const title = $('title').text();
+	const body = $('body').clone().find('script,style').remove().end().text();
+	return { title, body };
+};
+
+app.get('/summarize', async (c) => {
+	const url = c.req.query('url');
+	if (!url) return c.text('url param is required', 400);
+
+	const { title, body } = await parsePageContent(await getPage(url));
+
+	const { text: summary } = await generateText({
+		model: c.get('model'),
+		prompt: body,
+		system: 'summarize the cotnent of the provided page.',
+	});
+
+	return c.json({
+		summary,
+		title,
+		url,
+	});
+});
+
+export default app;
